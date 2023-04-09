faa85f9e7021aad525b7904e6fa157a1b6f3c3ba
Muntean Mihai <mihai.muntean@isa.utm.md>
2023-04-09 19:07:48 +0300

    Crearea entitatea personalului

---
+++ nightClub.BusinessLogic/DBModel/StaffContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nightClub.BusinessLogic.DBModel
+{
+    public class StaffContext : DbContext
+    {
+        public StaffContext() : base(
+            "name=nightClub")
+        {
+
+        }
+
+    }
+}
---
+++ nightClub.Domain/Entities/Staff/SDbTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nightClub.Domain.Entities.Staff
+{
+    public class SDbTable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Role { get; set; }
+        public string ImageUrl { get; set; }
+        public string ContactInfo { get; set; }
+        public double PayRate { get; set; }
+
+    }
+}
