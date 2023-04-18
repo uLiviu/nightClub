a872477e85044a03e20932934e29fa2c9bdcc6d8
Muntean Mihai <mihai.muntean@isa.utm.md>
2023-04-18 10:28:36 +0300

    Contact Model

---
+++ nightClub.Web/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nightClub.Web.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; } //defacto o sa citeasca numele de utilizator.
+        [Required]
+        public string Email { get; set; }// datele utilizatorului logat 
+        [Required]
+        [StringLength(30, MinimumLength = 8)]
+        public string Subject { get; set; }
+        [Required]
+        [StringLength(300, MinimumLength = 30)]
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
