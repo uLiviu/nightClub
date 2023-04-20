67c7a78be3c5ef5f64f745dff47b1270b6f1cd9d
Muntean Mihai <mihai.muntean@isa.utm.md>
2023-04-20 10:22:41 +0300

    Staff Modificare

--- nightClub.BusinessLogic/Implimentations/StaffBL.cs
+++ nightClub.BusinessLogic/Implimentations/StaffBL.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using nightClub.BusinessLogic.Core;
 using nightClub.BusinessLogic.Interfaces;
 
 namespace nightClub.BusinessLogic.Implimentations
 {
-    public class StaffBL: StaffApi, IStaff
+    public class StaffBL: UserApi, IStaff
     {
 
     }
