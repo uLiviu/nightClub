79265a0f803ee03ea654fce78a8d3d805e6b262d
Bagrin Andreea <andreea.bagrin@isa.utm.md>
2023-05-11 20:35:11 +0300

    Event: Crearea EventContext

---
+++ nightClub.BusinessLogic/DBModel/EventContext.cs
@@ -0,0 +1,18 @@
+﻿using nightClub.Domain.Entities.Event;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nightClub.BusinessLogic.DBModel
+{
+    public class EventContext : DbContext
+    {
+        public EventContext() : base("name=nightClub")
+        {
+        }
+        public virtual DbSet<EDbTable> Events { get; set; }
+    }
+}
