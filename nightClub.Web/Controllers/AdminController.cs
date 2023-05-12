981c05270c7a1bc305d569bbd51243008389fc30
Bagrin Andreea <andreea.bagrin@isa.utm.md>
2023-05-12 11:34:11 +0300

    Event: Booking List pentru Admin

--- nightClub.Web/Controllers/AdminController.cs
+++ nightClub.Web/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using nightClub.BusinessLogic.Interfaces;
+using nightClub.Domain.Entities.Ticket;
 using nightClub.Domain.Entities.User;
 using nightClub.Web.Filters;
 using nightClub.Web.Models;
@@ -12,10 +13,12 @@ namespace nightClub.Web.Controllers
     public class AdminController : BaseController
     {
         private readonly IUser _userBL;
+        private readonly ITicketBooking _ticketBL;
         public AdminController()
         {
             var bl = new BusinessLogic.BusinessLogic();
             _userBL = bl.GetUserBL();
+            _ticketBL = bl.GetTicketBookingBL();
         }
         // GET: Admin
         public ActionResult Index()
@@ -36,6 +39,26 @@ namespace nightClub.Web.Controllers
             if (user == null) return View("NotFound");
             _userBL.Delete(id);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Bookings()
+        {
+            SessionStatus();
+            var configure = new MapperConfiguration(cfg =>
+                cfg.CreateMap<TicketModel, Ticket>());
+            IMapper mapper = configure.CreateMapper();
+
+            var tickets = mapper.Map<List<Ticket>>(_ticketBL.GetTicketsList());
+            return View(tickets);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteTicketBooking(int id)
+        {
+            var ticket = _ticketBL.GetById(id);
+            if (ticket == null) return View("NotFound");
+            _ticketBL.Delete(id);
+            return RedirectToAction("Bookings");
         }
 
     }
