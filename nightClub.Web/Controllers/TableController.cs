c032710ded30a1cff1600a392e9caa1da310a3cf
Muntean Mihai <mihai.muntean@isa.utm.md>
2023-05-19 13:22:28 +0300

    Table: Booking View

---
+++ nightClub.Web/Controllers/TableController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nightClub.Web.Controllers
+{
+    public class TableController : BaseController
+    {
+        // GET: Table
+        public ActionResult Index()
+        {
+            SessionStatus();
+            return View();
+        }
+    }
+}
