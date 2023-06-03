7d05dbbdadfc83b35573e615971730fd59d752bc
Miron Anastasia <anastasia.miron@isa.utm.md>
2023-06-03 09:39:44 +0300

    Table: Initializarea constructorului

--- nightClub.Web/Controllers/TableController.cs
+++ nightClub.Web/Controllers/TableController.cs
@@ -8,6 +8,13 @@ namespace nightClub.Web.Controllers
 {
     public class TableController : BaseController
     {
+        private readonly ITable _tableBl;
+
+        public TableController()
+        {
+            var bl = new BusinessLogic.BusinessLogic();
+            _tableBl = bl.GetTableBL();
+        }
         // GET: Table
         public ActionResult Index()
         {
