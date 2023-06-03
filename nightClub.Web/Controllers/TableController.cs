4820ba3b652968dce69ada1977ad2e35d69c61c8
Miron Anastasia <anastasia.miron@isa.utm.md>
2023-06-03 10:22:25 +0300

    Table: Index

--- nightClub.Web/Controllers/TableController.cs
+++ nightClub.Web/Controllers/TableController.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using nightClub.BusinessLogic.Implimentations;
 using nightClub.BusinessLogic.Interfaces;
+using nightClub.Domain.Entities.Table;
+using nightClub.Web.Models;
 
 namespace nightClub.Web.Controllers
 {
     public class TableController : BaseController
     {
-        private readonly ITable _tableBl;
+        private readonly ITable _tableBL;
 
         public TableController()
         {
             var bl = new BusinessLogic.BusinessLogic();
-            _tableBl = bl.GetTableBL();
+            _tableBL = bl.GetTableBL();
         }
         // GET: Table
-        public ActionResult Index()
+        public ActionResult Index(string searchCriteria)
         {
             SessionStatus();
-            return View();
+            var configure = new MapperConfiguration(cfg =>
+                cfg.CreateMap<TableModel, Table>());
+            IMapper mapper = configure.CreateMapper();
+
+            var table = mapper.Map<List<Table>>(_tableBL.GetList(searchCriteria));
+            return View(table);
         }
     }
 }
