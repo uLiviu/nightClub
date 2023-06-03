09d34568dad352afb8351037deb6a5fab74b973a
Miron Anastasia <anastasia.miron@isa.utm.md>
2023-06-03 09:33:22 +0300

    Table: Adaugarea contextului spre baza de date si BL Models

---
+++ nightClub.BusinessLogic/DBModel/TableReservationContext.cs
@@ -0,0 +1,20 @@
+﻿using nightClub.Domain.Entities.Contact;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nightClub.Domain.Entities.Table;
+
+namespace nightClub.BusinessLogic.DBModel
+{
+    public class TableReservationContext : DbContext
+    {
+        public TableReservationContext() :
+            base("name=nightClub")
+        {
+        }
+        public virtual DbSet<TRDbTable> Reservations { get; set; }
+    }
+}
