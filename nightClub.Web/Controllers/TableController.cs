71e779cbc0deaa2090fb446bb17ee57db0f0e103
Miron Anastasia <anastasia.miron@isa.utm.md>
2023-06-03 10:33:46 +0300

    Table: Reservation ActionResult și View, Confirmation View

--- nightClub.Web/Controllers/TableController.cs
+++ nightClub.Web/Controllers/TableController.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using nightClub.BusinessLogic.Implimentations;
 using nightClub.BusinessLogic.Interfaces;
 using nightClub.Domain.Entities.Table;
+using nightClub.Web.Filters;
 using nightClub.Web.Models;
 
 namespace nightClub.Web.Controllers
@@ -30,6 +31,47 @@ namespace nightClub.Web.Controllers
 
             var table = mapper.Map<List<Table>>(_tableBL.GetList(searchCriteria));
             return View(table);
+        }
+
+        // GET: Table\Reservation
+        public ActionResult Reservation()
+        {
+            SessionStatus();
+            if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
+                return RedirectToAction("Index", "Login");
+
+            Table model = new Table();
+            model.Username = ViewBag.CurrentUser.Username;
+            model.Email = ViewBag.CurrentUser.Email;
+            model.Phone = ViewBag.CurrentUser.PhoneNumb;
+            model.GuestsNumber = 1;
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Reservation(Table table)
+        {
+            SessionStatus();
+            if (ModelState.IsValid)
+            {
+                var config = new MapperConfiguration(cfg =>
+                    cfg.CreateMap<Table, TableModel>());
+                IMapper mapper = config.CreateMapper();
+                var tableModel = mapper.Map<TableModel>(table);
+
+                var createReservation = _tableBL.Add(tableModel);
+
+                if (createReservation.Status)
+                {
+                    return View("Confirmation", table);
+                }
+                else
+                {
+                    ModelState.AddModelError("", createReservation.StatusMsg);
+                    return View();
+                }
+            }
+            return View();
         }
     }
 }
