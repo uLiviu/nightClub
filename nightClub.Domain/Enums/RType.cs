fce17d3d9c46f0543cf8d177fe6bedb79f7f5c78
Miron Anastasia <anastasia.miron@isa.utm.md>
2023-06-03 09:26:02 +0300

    Table: Adăugarea Modelului

---
+++ nightClub.Domain/Enums/RType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nightClub.Domain.Enums
+{
+    public enum RType
+    {
+        VIP = 0,
+        General,
+        Booth,
+        Outdoor, //an outdoor area
+        Group    //a reserved area with multiple tables, along with perks like priority entry, complimentary drinks, or special promotions. 
+    }
+}
---
+++ nightClub.Web/Models/Table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using nightClub.Domain.Enums;
+
+namespace nightClub.Web.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Your Name")]
+        public string Username { get; set; }
+        [Required]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [Display(Name = "#of Guests")]
+        [Range(1, 12, ErrorMessage = "Between 1 and 12 guests.")]
+        public int GuestsNumber { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Reservation { get; set; }
+
+        [Required]
+        [Display(Name = "Reservation Type")]
+        public RType ReservationType { get; set; }
+
+        [Display(Name = "Any special requests")]
+        public string Description { get; set; }
+    }
+}
