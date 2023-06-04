2898403e42e6ad74ca54b89866aac135cae18184
uLiviu <liviu.ungureanu@isa.utm.md>
2023-06-04 12:01:26 +0300

    Bar: Adaugare Modele

---
+++ nightClub.Domain/Entities/Bar/BarDbTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nightClub.Domain.Entities.Bar
+{
+    public class BarDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Alcohol { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ nightClub.Web/Models/Bar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nightClub.Web.Models
+{
+    public class Bar
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public int Alcohol { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
